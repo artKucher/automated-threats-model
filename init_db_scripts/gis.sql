89c1584608f67449da5d41450d5905cacc81cd12
artemkucher <kuch.art@mail.ru>
2022-06-13 15:59:52 +0500

    Fixes

--- init_db_scripts/gis.sql
+++ init_db_scripts/gis.sql
@@ -1,63 +1,63 @@
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (1, 'REGIONAL', 'BA', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (2, 'REGIONAL', 'BA', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (3, 'REGIONAL', 'BA', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (4, 'REGIONAL', 'FI', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (5, 'REGIONAL', 'FI', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (6, 'REGIONAL', 'FI', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (7, 'REGIONAL', 'HA', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (8, 'REGIONAL', 'HA', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (9, 'REGIONAL', 'HA', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (10, 'REGIONAL', 'HA', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (11, 'REGIONAL', 'HA', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (12, 'REGIONAL', 'HA', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (13, 'REGIONAL', 'CO', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (14, 'REGIONAL', 'CO', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (15, 'REGIONAL', 'CO', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (16, 'REGIONAL', 'DE', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (17, 'REGIONAL', 'DE', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (18, 'REGIONAL', 'DE', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (19, 'REGIONAL', 'IM', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (20, 'REGIONAL', 'IM', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (21, 'REGIONAL', 'IM', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (22, 'FEDERAL', 'BA', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (23, 'FEDERAL', 'BA', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (24, 'FEDERAL', 'BA', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (25, 'FEDERAL', 'FI', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (26, 'FEDERAL', 'FI', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (27, 'FEDERAL', 'FI', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (28, 'FEDERAL', 'HA', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (29, 'FEDERAL', 'HA', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (30, 'FEDERAL', 'HA', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (31, 'FEDERAL', 'HA', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (32, 'FEDERAL', 'HA', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (33, 'FEDERAL', 'HA', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (34, 'FEDERAL', 'CO', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (35, 'FEDERAL', 'CO', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (36, 'FEDERAL', 'CO', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (37, 'FEDERAL', 'DE', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (38, 'FEDERAL', 'DE', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (39, 'FEDERAL', 'DE', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (40, 'FEDERAL', 'IM', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (41, 'FEDERAL', 'IM', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (42, 'FEDERAL', 'IM', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (43, 'OBJECTED', 'BA', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (44, 'OBJECTED', 'BA', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (45, 'OBJECTED', 'BA', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (46, 'OBJECTED', 'FI', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (47, 'OBJECTED', 'FI', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (48, 'OBJECTED', 'FI', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (49, 'OBJECTED', 'HA', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (50, 'OBJECTED', 'HA', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (51, 'OBJECTED', 'HA', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (52, 'OBJECTED', 'HA', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (53, 'OBJECTED', 'HA', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (54, 'OBJECTED', 'HA', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (55, 'OBJECTED', 'CO', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (56, 'OBJECTED', 'CO', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (57, 'OBJECTED', 'CO', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (58, 'OBJECTED', 'DE', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (59, 'OBJECTED', 'DE', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (60, 'OBJECTED', 'DE', 'K3');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (61, 'OBJECTED', 'IM', 'K1');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (62, 'OBJECTED', 'IM', 'K2');
-INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (63, 'OBJECTED', 'IM', 'K3');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (1, 'REGIONAL', 'K1', 'BASIC');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (2, 'REGIONAL', 'K2', 'BASIC');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (3, 'REGIONAL', 'K3', 'BASIC');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (4, 'REGIONAL', 'K1', 'FINANCIAL');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (5, 'REGIONAL', 'K2', 'FINANCIAL');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (6, 'REGIONAL', 'K3', 'FINANCIAL');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (7, 'REGIONAL', 'K1', 'HAS_PUBLIC_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (8, 'REGIONAL', 'K2', 'HAS_PUBLIC_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (9, 'REGIONAL', 'K3', 'HAS_PUBLIC_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (10, 'REGIONAL', 'K1', 'HAS_PRIVATE_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (11, 'REGIONAL', 'K2', 'HAS_PRIVATE_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (12, 'REGIONAL', 'K3', 'HAS_PRIVATE_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (13, 'REGIONAL', 'K1', 'COMMUNICATION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (14, 'REGIONAL', 'K2', 'COMMUNICATION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (15, 'REGIONAL', 'K3', 'COMMUNICATION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (16, 'REGIONAL', 'K1', 'DEFENCE_SYSTEM');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (17, 'REGIONAL', 'K2', 'DEFENCE_SYSTEM');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (18, 'REGIONAL', 'K3', 'DEFENCE_SYSTEM');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (19, 'REGIONAL', 'K1', 'IMPACT_ON_PUBLIC_PERCEPTION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (20, 'REGIONAL', 'K2', 'IMPACT_ON_PUBLIC_PERCEPTION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (21, 'REGIONAL', 'K3', 'IMPACT_ON_PUBLIC_PERCEPTION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (22, 'FEDERAL', 'K1', 'BASIC');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (23, 'FEDERAL', 'K2', 'BASIC');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (24, 'FEDERAL', 'K3', 'BASIC');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (25, 'FEDERAL', 'K1', 'FINANCIAL');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (26, 'FEDERAL', 'K2', 'FINANCIAL');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (27, 'FEDERAL', 'K3', 'FINANCIAL');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (28, 'FEDERAL', 'K1', 'HAS_PUBLIC_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (29, 'FEDERAL', 'K2', 'HAS_PUBLIC_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (30, 'FEDERAL', 'K3', 'HAS_PUBLIC_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (31, 'FEDERAL', 'K1', 'HAS_PRIVATE_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (32, 'FEDERAL', 'K2', 'HAS_PRIVATE_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (33, 'FEDERAL', 'K3', 'HAS_PRIVATE_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (34, 'FEDERAL', 'K1', 'COMMUNICATION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (35, 'FEDERAL', 'K2', 'COMMUNICATION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (36, 'FEDERAL', 'K3', 'COMMUNICATION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (37, 'FEDERAL', 'K1', 'DEFENCE_SYSTEM');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (38, 'FEDERAL', 'K2', 'DEFENCE_SYSTEM');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (39, 'FEDERAL', 'K3', 'DEFENCE_SYSTEM');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (40, 'FEDERAL', 'K1', 'IMPACT_ON_PUBLIC_PERCEPTION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (41, 'FEDERAL', 'K2', 'IMPACT_ON_PUBLIC_PERCEPTION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (42, 'FEDERAL', 'K3', 'IMPACT_ON_PUBLIC_PERCEPTION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (43, 'OBJECTED', 'K1', 'BASIC');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (44, 'OBJECTED', 'K2', 'BASIC');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (45, 'OBJECTED', 'K3', 'BASIC');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (46, 'OBJECTED', 'K1', 'FINANCIAL');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (47, 'OBJECTED', 'K2', 'FINANCIAL');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (48, 'OBJECTED', 'K3', 'FINANCIAL');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (49, 'OBJECTED', 'K1', 'HAS_PUBLIC_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (50, 'OBJECTED', 'K2', 'HAS_PUBLIC_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (51, 'OBJECTED', 'K3', 'HAS_PUBLIC_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (52, 'OBJECTED', 'K1', 'HAS_PRIVATE_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (53, 'OBJECTED', 'K2', 'HAS_PRIVATE_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (54, 'OBJECTED', 'K3', 'HAS_PRIVATE_WEB_RESOURCES');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (55, 'OBJECTED', 'K1', 'COMMUNICATION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (56, 'OBJECTED', 'K2', 'COMMUNICATION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (57, 'OBJECTED', 'K3', 'COMMUNICATION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (58, 'OBJECTED', 'K1', 'DEFENCE_SYSTEM');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (59, 'OBJECTED', 'K2', 'DEFENCE_SYSTEM');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (60, 'OBJECTED', 'K3', 'DEFENCE_SYSTEM');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (61, 'OBJECTED', 'K1', 'IMPACT_ON_PUBLIC_PERCEPTION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (62, 'OBJECTED', 'K2', 'IMPACT_ON_PUBLIC_PERCEPTION');
+INSERT INTO public.app_gisclass (id, size, protection_class, specification) VALUES (63, 'OBJECTED', 'K3', 'IMPACT_ON_PUBLIC_PERCEPTION');
