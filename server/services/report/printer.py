f266e0774f0705587482421f47b46fcb426c4add
artemkucher <kuch.art@mail.ru>
2022-06-17 12:15:41 +0500

    Add charts to report

--- server/services/report/printer.py
+++ server/services/report/printer.py
@@ -1,10 +1,15 @@
+import io
+import random
 from io import BytesIO
 from typing import Any
 from typing import List as ListType
 
-from odf.style import Style, TextProperties, ListLevelProperties
-from odf.text import P, Span, H, ListStyle, List, ListItem, ListLevelStyleNumber
+from matplotlib import pyplot as plt
+from odf.draw import Image, Frame
+from odf.style import Style, TextProperties, ListLevelProperties, GraphicProperties
+from odf.text import P, H, ListStyle, List, ListItem, ListLevelStyleNumber
 
+from app.models import NegativeConsequenceGroup, AssetType
 from services.report.report import Report
 from odf.opendocument import OpenDocumentText
 
@@ -28,6 +33,12 @@ class ReportPrinter:
         numberedlistproperty.addElement(ListLevelProperties(
             minlabelwidth="%fcm" % (level - .2)))
         self.numberedliststyle.addElement(numberedlistproperty)
+
+        image_style = Style(name='frstyle', parentstylename="Graphics", family="graphic")
+        image_style.addElement(GraphicProperties(verticalrel="paragraph",
+                                             horizontalrel="paragraph"))
+        self.image_style = image_style
+        self.document.automaticstyles.addElement(image_style)
 
         self.document.styles.addElement(self.h1style)
         self.document.styles.addElement(self.h2style)
@@ -83,16 +94,132 @@ class ReportPrinter:
 
     def generate_main_page(self):
         system_header = H(outlinelevel=1, stylename=self.h1style, text=f"Cистема: {self.report.system.name}")
+        self.document.text.addElement(system_header)
+        self.add_ispdn_classes_info()
+        self.add_gis_classes_info()
+        self.add_asutp_classes_info()
+        self.add_kii_classes_info()
+
+        self.document.text.addElement(
+            H(outlinelevel=1, stylename=self.h2style, text=f"Объектов: {self.report.assets.count()} шт.")
+        )
+        labels = []
+        sizes = []
+        for type in AssetType.objects.all():
+            assets_count = self.report.system.assets.filter(asset_type=type).count()
+            if assets_count:
+                labels.append(str(type))
+                sizes.append(
+                    assets_count
+                )
+
+        chart = self.draw_chart(
+            labels=labels,
+            sizes=sizes,
+        )
+        self.add_chart(chart)
+
+        self.document.text.addElement(
+            H(
+                outlinelevel=1,
+                stylename=self.h2style,
+                text=f"Негативных последствий: {self.report.negative_consequences.count()} шт."
+            )
+        )
+        labels = []
+        sizes = []
+        for group in NegativeConsequenceGroup.objects.all():
+            labels.append(str(group))
+            group_negative_consequences_count = self.report.system.negative_consequences.filter(group=group).count()
+            sizes.append(
+                group_negative_consequences_count
+            )
+        chart = self.draw_chart(
+            labels=labels,
+            sizes=sizes,
+        )
+        self.add_chart(chart)
+
+
         assets_block_header = H(outlinelevel=1, stylename=self.h2style, text=f"Объекты:")
         assets_list = self.generate_numbered_list(self.report.assets)
         negative_consequences_block_header = H(outlinelevel=1, stylename=self.h2style, text=f"Негативные последствия:")
         negative_consequences_list = self.generate_numbered_list(self.report.negative_consequences)
 
-        self.document.text.addElement(system_header)
         self.document.text.addElement(assets_block_header)
         self.document.text.addElement(assets_list)
         self.document.text.addElement(negative_consequences_block_header)
         self.document.text.addElement(negative_consequences_list)
+
+    def add_chart(self, chart):
+        photoframe = Frame(width="260pt", height="200pt", stylename=self.image_style)
+        href = self.document.addPicture(filename=str(random.randint(0, 9999)), content=chart)
+        photoframe.addElement(Image(href=href))
+        p = P()
+        p.addElement(photoframe)
+        self.document.text.addElement(p)
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+        self.document.text.addElement(P(text=''))
+
+    def draw_chart(self, labels, sizes):
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        buf = io.BytesIO()
+        plt.savefig(buf)
+        return buf.getvalue()
+
+    def add_ispdn_classes_info(self):
+        if self.report.system.ispdn_classes.exists():
+            ispdn_specifications = [
+                str(ispdn.get_specification_display()) for ispdn in self.report.system.ispdn_classes.all()
+            ]
+            ispdn_type = self.report.system.ispdn_classes.all()[0].get_type_display()
+            ispdn_protection_level = self.report.system.ispdn_classes.all()[0].get_protection_level_display()
+            text = f"ИСПДН: {ispdn_type}, {ispdn_protection_level}, {'; '.join(ispdn_specifications)}"
+            self.document.text.addElement(P(text=text))
+
+    def add_gis_classes_info(self):
+        if self.report.system.gis_classes.exists():
+            gis_specifications = [
+                str(gis.get_specification_display()) for gis in self.report.system.gis_classes.all()
+            ]
+            gis_size = self.report.system.gis_classes.all()[0].get_size_display()
+            gis_protection_class = self.report.system.gis_classes.all()[0].get_protection_class_display()
+            text = f"ГИС: {gis_specifications}, {gis_size}, {';'.join(gis_protection_class)}"
+            self.document.text.addElement(P(text=text))
+
+    def add_asutp_classes_info(self):
+        if self.report.system.asutp_classes.exists():
+            asutp_specifications = [
+                str(asutp.get_specification_display()) for asutp in self.report.system.asutp_classes.all()
+            ]
+            asutp_protection_class = self.report.system.gis_classes.all()[0].get_protection_class_display()
+            text = f"АСУТП: {asutp_protection_class}, {';'.join(asutp_specifications)}"
+            self.document.text.addElement(P(text=text))
+
+    def add_kii_classes_info(self):
+        if self.report.system.kii_classes.exists():
+            kii_specifications = [
+                str(kii.get_specification_display()) for kii in self.report.system.kii_classes.all()
+            ]
+            kii_significance_attribute = self.report.system.kii_classes.all()[0].get_significance_attribute_display()
+            text = f"КИИ: {kii_significance_attribute}, {';'.join(kii_specifications)}"
+            self.document.text.addElement(P(text=text))
 
     def generate_numbered_list(self, collection: ListType[Any]):
         assets_list = List(stylename=self.numberedliststyle)
